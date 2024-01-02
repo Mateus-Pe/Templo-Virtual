9b7a8a48795fb3a003b71c1a4b8d417738e40c9f
filipe <fam.machado@gmail.com>
2024-01-02 14:30:52 -0300

    layout dinamico sem formatacao

--- scripts/configurar-layout.js
+++ scripts/configurar-layout.js
@@ -26,8 +26,8 @@ evento_agenda();
   function evento_agenda(){
     $.ajax({
       method: "POST",
-      url: "https://pedeoferta.com.br/templo/index.php/welcome/get_evento_agenda",
-      
+      url: "https://pedeoferta.com.br/templo/index.php/welcome/get_layout_evento",
+      data: {evento_id: "1"}
     })
     .done(function(ret) {
 
@@ -36,10 +36,10 @@ evento_agenda();
       var html = '';
       html += '<section class="regular slider">';
 
-      $.each(obj.lista_evento_agenda, function (k, lpp) {
-          html += '<a data-evento_cod="'+lpp.evento_id+'" class="produtos_perfil"><div data-evento_cod="'+lpp.evento_id+'" class="divPerfilEC" style="opacity: 0.5;height: 90px; display: flex;align-items: center; flex-direction: row;flex-wrap: wrap; justify-content: center;">';
+      $.each(obj.lista_layout_evento, function (k, lpp) {
+          html += '<a data-img_background="'+lpp.layout_background+'" data-evento_css="'+lpp.layout_evento_css+'" data-data_css="'+lpp.layout_data_css+'" data-dia_css="'+lpp.layout_dia_css+'" data-mes_css="'+lpp.layout_mes_css+'" data-hora_css="'+lpp.layout_hora_css+'" class="layout_css"><div data-evento_cod="'+lpp.evento_id+'" class="divPerfilEC" style="opacity: 0.5;height: 90px; display: flex;align-items: center; flex-direction: row;flex-wrap: wrap; justify-content: center;">';
               html += '<div style="display: grid;">';
-          html += '<div style="display: flex;align-items: center; flex-direction: row;flex-wrap: wrap; justify-content: center;"><img  src="'+lpp.evento_icone_img+'" style="height:50px"/></div>';
+          html += '<div style="display: flex;align-items: center; flex-direction: row;flex-wrap: wrap; justify-content: center;"><img  src="'+lpp.layout_background_icone+'" style="height:50px"/></div>';
                 html += '<span style="font-size: 1.3rem; text-align:center; text-decoration:none;"></span></div>';
               html += '</div></a>';
       });
@@ -49,15 +49,28 @@ evento_agenda();
 
       slick();
       $('#carregando').hide();
-      $('.produtos_perfil').click(function(e){
-        $('.divPerfilEC').removeClass('perfil_ec_selected');
-        $(this).children().addClass('perfil_ec_selected');
-                    
-        atual_evento_cod = $(this).data('evento_cod');
-        console.log(atual_evento_cod);
+      $('.layout_css').click(function(e){
+         $("#divImg").css("background-image", "url("+$(this).data('img_background')+")");
+         set_style($(this).data('evento_css'), 'evento');
+         set_style($(this).data('data_css'), 'data');
+         set_style($(this).data('dia_css'), 'dia');
+         set_style($(this).data('mes_css'), 'mes');
+         set_style($(this).data('hora_css'), 'hora');
       });
     });
   }
+
+
+  function set_style(strStyle, divElement){
+      arrStyle = strStyle.split(";");
+      $.each(arrStyle, function(index, value) { 
+        var hashmap = value.split(":");
+        $("#"+divElement).css(hashmap[0], hashmap[1]);        
+    });
+
+
+  }
+
 
   function slick(){
     $(".regular").slick({
