3dff249285115a0eeb009d9c8a398c3746d524bf
Mateus <mateuclash@gmail.com>
2024-01-19 10:44:13 -0300

    dinamica do feed

--- scripts/feed.js
+++ scripts/feed.js
@@ -2,8 +2,8 @@ evento_agenda();
 function evento_agenda(){
     $.ajax({
       method: "POST",
-      url: "https://pedeoferta.com.br/templo/index.php/welcome/get_layout_evento",
-      data: {evento_id: "1"}
+      url: "https://pedeoferta.com.br/templo/index.php/welcome/get_feed",
+     
     })
     .done(function(ret) {
      
@@ -11,26 +11,26 @@ function evento_agenda(){
       var html = '';
      
       console.log(obj);
-      $.each(obj.lista_layout_evento, function (k, lpp) {
+      $.each(obj.lista_feed, function (k, lpp) {
         html += '<div class="div_publicacao">';
         html += '<div class="feed_principal">';
         html += '<div class="div_feed_secundario">';
         html += '<div>';
         html += '<div>';
         html += '<a class="div_perfil">';
-        html += '<img class="img_igreja" src="./imgs/imgs-igreja/igreja.png">';
+        html += '<img class="img_igreja" src="'+lpp.igreja_logo+'">';
         html += '<span class="nome_igreja">';
-        html += 'Catedral Nossa Senhora dos Prazeres';
+        html += lpp.igreja_nome;
         html += '</span>';
         html += '</a>';
         html += '</div>';
         html += '<div class="div_layout_feed">';
         html += '<a class="a_img_layout">';
-        html += '<img class="img_layout_feed" src="./imgs/imgs-igreja/missa1.jpg">';
+        html += '<img class="img_layout_feed" src="'+lpp.agenda_img+'">';
         html += '</a>';
         html += '<div class="div_descricao">';
         html += '<span class="span_descricao">';
-        html += 'Descrição do evento';
+        html += lpp.descricao_evento;
         html += '</span>';
         html += '</div>';
         html += '</div>';
