fe836524662c612db9255db75b697e7d2a00123b
filipe <fam.machado@gmail.com>
2024-01-30 12:07:49 -0300

    inclusão da çogica de login

---
+++ scripts/loginas.js
@@ -0,0 +1,43 @@
+var searchParams = new URLSearchParams(window.location.search);
+var login = searchParams.get("c");
+
+$('#login').val(login);
+
+$('#btn_seguir').click(function(e){
+  console.log('entro');
+  if($('#login').val().length < 9){
+      $('.cadastro__alert').text('Digite seu DDD + Celular para prosseguir');
+  }
+  else{
+     loginas();
+  }
+});
+
+function loginas(){
+  var dados = {
+    usuario_celular: $('#login').val(),
+    usuario_senha: $('#senha').val()
+
+  };
+
+  $.ajax({
+    type: "POST",
+    url: "https://pedeoferta.com.br/templo/index.php/welcome/login",
+    data: dados,
+    dataType: "json",
+    success: function (response) {
+ 
+    console.log(response.status);
+      if(response.status == 1) {
+        window.sessionStorage.setItem('paroquia_id', response.usuario.usuario_paroquia_id);
+        window.location = "lista-igreja.html";
+        
+      }
+
+    },
+    error: function (error) {
+      console.log(error);
+    }
+  });
+}
+ 
