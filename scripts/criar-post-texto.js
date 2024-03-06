6f21a178d2ebb8deb115431f804a55e0031ccce6
Mateus <mateuclash@gmail.com>
2024-03-06 17:02:33 -0300

    colocando o "mini word" no textarea da tela criar post texto

--- scripts/criar-post-texto.js
+++ scripts/criar-post-texto.js
@@ -1,3 +1,5 @@
+var textoPadrao = 'Clique para editar o texto';
+
 $('#imagem').click(function(e){
     window.location = "criar-post.html";
   });
@@ -6,44 +8,133 @@ $('#imagem').click(function(e){
     window.location = "criar-post-video.html";
   });
 
-  $(function() {
-    $('#feed_texto').click(function(e){
-        $('#modal_texto').show();
+    document.getElementById('feed_texto').addEventListener('click', function() {
+    document.querySelector('#btn_salvar').style.display = 'none';
+    document.querySelector('#divHeader').style.display = 'none';
+    document.querySelector('#editor_desc').style.display = 'none';
+    document.querySelector('#editor_texto').style.display = 'flex';
+    document.querySelector('#header').style.display = 'flex';
+    });
+    
+    document.getElementById('descricao_texto').addEventListener('click', function() {
+    document.querySelector('#btn_salvar').style.display = 'none';
+    document.querySelector('#divHeader').style.display = 'none';
+    document.querySelector('#editor_texto').style.display = 'none';
+    document.querySelector('#header').style.display = 'flex';
+    document.querySelector('#editor_desc').style.display = 'flex';
     });
 
-    $('#btn_ok').click(function(e){
-         $('#modal_texto').hide();
-
+    document.getElementById('salva_publi').addEventListener('click', function() {
+    document.querySelector('#btn_salvar').style.display = 'inline';
+    document.querySelector('#divHeader').style.display = 'flex';
+    document.querySelector('#editor_texto').style.display = 'none';
+    document.querySelector('#editor_desc').style.display = 'none';
+    document.querySelector('#header').style.display = 'none';
     });
 
-    var $exibirTexto = $("#feed_texto");
-    $("#texto_publi").on("keyup", function () {
-        var texto = $(this).val().trim(); 
-        if (texto !== "") {
-            $exibirTexto.text(texto);
-        } else {
-            $exibirTexto.text("Clique para editar o texto");
-        }
-    });
-});
 
-$(function() {
-  $('#descricao_texto').click(function(e){
-      $('#modal_descricao').show();
-  });
 
-  $('#btn_ok1').click(function(e){
-       $('#modal_descricao').hide();
 
-  });
+ClassicEditor
+    .create( document.querySelector( '#texto_publi' ), {
+        toolbar: {
+            items: [
+                'heading',
+                '|',
+                'bold',
+                'italic',
+                'underline',
+                'strikethrough',
+                '|',
+                'link',
+                'bulletedList',
+                'numberedList',
+                '|',
+                'outdent',
+                'indent',
+                '|',
+                'blockQuote',
+                '|',
+                'undo',
+                'redo'
+            ]
+        },
+        language: 'pt-br'
+    } )
+    .then( editor => {
+        // Armazena a instância do editor em uma variável
+        window.editor = editor;
 
-  var $exibirTexto = $(".span_descricao");
-  $("#descricao_publi").on("keyup", function () {
-      var texto = $(this).val().trim(); 
-      if (texto !== "") {
-          $exibirTexto.text(texto);
-      } else {
-          $exibirTexto.text("Clique para adicionar uma descrição");
-      }
-  });
-});
+        // Armazena o conteúdo anterior do editor
+        let conteudoAnterior = '';
+
+        // Adiciona um ouvinte de evento para detectar mudanças no conteúdo do editor
+        editor.model.document.on( 'change:data', () => {
+            // Obtém o conteúdo HTML atual do editor
+            var conteudoHTML = editor.getData();
+            
+            // Verifica se o conteúdo está vazio
+            if (conteudoHTML.trim() === '') {
+                // Se estiver vazio, restaura o texto padrão
+                document.getElementById('feed_texto').innerHTML = textoPadrao;
+            } else {
+                // Se não estiver vazio, atualiza o conteúdo da div de exibição com o novo conteúdo
+                document.getElementById('feed_texto').innerHTML = conteudoHTML;
+                // Atualiza o conteúdo anterior
+                conteudoAnterior = conteudoHTML;
+            }
+        } );
+    } )
+    .catch( error => {
+        console.error( error );
+    } );
+
+
+
+    ClassicEditor
+    .create( document.querySelector( '#descricao_publi' ), {
+        toolbar: {
+            items: [
+                'heading',
+                '|',
+                'bold',
+                'italic',
+                'underline',
+                'strikethrough',
+                '|',
+                'link',
+                'bulletedList',
+                'numberedList',
+                '|',
+                'outdent',
+                'indent',
+                '|',
+                'blockQuote',
+                '|',
+                'undo',
+                'redo'
+            ]
+        },
+        language: 'pt-br'
+    } )
+    .then( editor => {
+        // Armazena a instância do editor em uma variável
+        window.descricaoEditor = editor;
+
+        // Adiciona um ouvinte de evento para detectar mudanças no conteúdo do editor
+        editor.model.document.on( 'change:data', () => {
+            // Obtém o conteúdo HTML atual do editor
+            var descricaoHTML = editor.getData();
+            
+            // Atualiza o conteúdo da div de exibição com o conteúdo HTML do editor
+            document.getElementById('descricao_texto').innerHTML = descricaoHTML;
+            
+            // Se o editor estiver vazio, exibe o texto padrão na div
+            if (descricaoHTML.trim() === "") {
+                document.getElementById('descricao_texto').textContent = "Clique para adicionar uma descrição";
+            }
+        } );
+    } )
+    .catch( error => {
+        console.error( error );
+    } );
