5c78f6e2d899bcc50f1e74873d1896bcdf56fa77
filipe <fam.machado@gmail.com>
2024-07-04 19:04:18 -0300

    horario no feed

--- scripts/feed.js
+++ scripts/feed.js
@@ -1,5 +1,30 @@
 var players = {};
 
+const dias = [
+  { 'id': 1, 'name': 'Segunda' },
+  { 'id': 2, 'name': 'Terça' },
+  { 'id': 3, 'name': 'Quarta' },
+  { 'id': 4, 'name': 'Quinta' },
+  { 'id': 5, 'name': 'Sexta' },
+  { 'id': 6, 'name': 'Sábado' },
+  { 'id': 0, 'name': 'Domingo' },
+];
+
+const months = [
+  { 'id': 1, 'name': 'Jan' },
+  { 'id': 2, 'name': 'Fev' },
+  { 'id': 3, 'name': 'Mar' },
+  { 'id': 4, 'name': 'Abr' },
+  { 'id': 5, 'name': 'Mai' },
+  { 'id': 6, 'name': 'Jun' },
+  { 'id': 7, 'name': 'Jul' },
+  { 'id': 8, 'name': 'Ago' },
+  { 'id': 9, 'name': 'Set' },
+  { 'id': 10, 'name': 'Out' },
+  { 'id': 11, 'name': 'Nov' },
+  { 'id': 12, 'name': 'Dez' },
+];
+
 window.onload = function() {
   var descricao = "Descrição da publicação";
   var url = window.location.href; 
@@ -59,10 +84,10 @@ function montaHtml(linha, i) {
 
   html += '<div style="border: 1px solid black; align-items: center; display: grid; padding-right: 10px; padding-left: 10px; font-family: exo;">';
   html += '<span id="dia_evento" style="font-size: 1.2rem; color: grey; display: flex; justify-content: center;">';
-  html += 'Amanhã';
+  html += formata_dia(linha.data_evento);
   html += '</span>';
   html += '<span id="hora_evento" style="font-size: 1.2rem; color: grey; display: flex; justify-content: center;">';
-  html += '19h';
+  html += formata_hora(linha.data_evento);
   html += '</span>';
   html += '</div>';
 
@@ -451,4 +476,63 @@ function extrairIdDaImagem(imagemUrl) {
   } else {
     return null;
   }
+}
+
+function formata_hora(data){
+  //var data =  window.sessionStorage.getItem('data_referencia');
+  var hashdata = data.split("-");
+  ano = hashdata[0];
+  mes = hashdata[1];
+  D = hashdata[2];
+  H = hashdata[3];
+  hoje = new Date();
+  S = new Date(ano,mes -1,D).getUTCDay();
+  date_ref = new  Date(ano,mes -1,D);
+  diff = new Date(date_ref - hoje);
+  diff_days = diff/1000/60/60/24;
+
+
+    strHora = H;
+    return strHora;
+}
+
+
+
+function formata_dia(data){
+  //var data =  window.sessionStorage.getItem('data_referencia');
+  var hashdata = data.split("-");
+  ano = hashdata[0];
+  mes = hashdata[1];
+  D = hashdata[2];
+  H = hashdata[3];
+  hoje = new Date();
+  S = new Date(ano,mes -1,D).getUTCDay();
+  date_ref = new  Date(ano,mes -1,D);
+  diff = new Date(date_ref - hoje);
+  diff_days = diff/1000/60/60/24;
+ 
+  diaName = dias.find(x => x.id === S).name;
+  mes = months.find(x => x.id == mes).name;
+
+  strDia = "";
+  if(Math.floor(diff_days) < 7){
+    if(Math.floor(diff_days) < 2){
+      if(D == hoje.getDate()){
+        strDia = 'Hoje';
+      }
+      if(parseInt(D) == parseInt(hoje.getDate()) + 1){
+        strDia = 'Amanhã';
+      }
+    }else{
+      strDia = diaName;
+    }
+  }
+    if(parseInt(D) < 10){
+      D = '0'+ parseInt(D);
+    }
+    if(strDia == "")
+      strDia = D+"/"+mes;
+
+    return strDia;
+
 }
