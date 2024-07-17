c7e338bc5dce8c3e5ff4205c0c39abbc0af40723
Mateus <mateuclash@gmail.com>
2024-07-17 18:26:53 -0300

    teste 2 da tela principal

--- scripts/principal.js
+++ scripts/principal.js
@@ -22,24 +22,41 @@ function eventos_principal(){
         $.each(evento.agendas, function (k, agenda) {
         html +=        '<div id="agenda" class="agenda">';
         html +=             '<div>';
+        html +=             '<div class="div_agenda">'
+        html +=                 '<span class="span_agenda span_agenda_left">';
+        html +=                     formata_dia_resumido(agenda.agenda_horario);
+        html +=                 '</span>';
+        html +=                 '<span class="span_agenda span_agenda_right">';
+        html +=                     formata_hora_resumido(agenda.agenda_horario);
+        html +=                 '</span>';
+        html +=             '</div>'
         html +=                 '<img id="div_img" src="'+agenda.agenda_img+'">';
         html +=             '</div>';
         html +=             '<div id="descricao">';
         html +=                 '<span class="span_nome_igreja">';
         html +=                     agenda.igreja_nome;
         html +=                 '</span>';
-        html +=                 '<span class="span_agenda">';
-        html +=                     formata_dia(agenda.agenda_horario) +' ás '+ formata_hora(agenda.agenda_horario);
-        html +=                 '</span>';
         html +=             '</div>';
         html +=         '</div>';
         });
-
+        html +=      '</div>';
+        html +=      '<div class="div_ver_mais">';
+        html +=         '<span>';
+        html +=             'Ver mais';
+        html +=         '</span>';
         html +=      '</div>';
         html += '</div>';
         
         
       });
       $("#eventos").html(html);
+      configurarEventos();
     });
+}
+
+
+function configurarEventos(){
+  $(".div_ver_mais").click(function(){
+    location.href = "calendario-feed.html";
+  });
 }
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,30 +1,30 @@
 const dias = [
-    { 'id': 1, 'name': 'Segunda' },
-    { 'id': 2, 'name': 'Terça' },
-    { 'id': 3, 'name': 'Quarta' },
-    { 'id': 4, 'name': 'Quinta' },
-    { 'id': 5, 'name': 'Sexta' },
-    { 'id': 6, 'name': 'Sábado' },
-    { 'id': 0, 'name': 'Domingo' },
+    { 'id': 1, 'name': 'Segunda', 'name_caps' : 'SEGUNDA' },
+    { 'id': 2, 'name': 'Terça', 'name_caps' : 'TERÇA' },
+    { 'id': 3, 'name': 'Quarta', 'name_caps' : 'QUARTA' },
+    { 'id': 4, 'name': 'Quinta', 'name_caps' : 'QUINTA' },
+    { 'id': 5, 'name': 'Sexta', 'name_caps' : 'SEXTA' },
+    { 'id': 6, 'name': 'Sábado', 'name_caps' : 'SÁBADO' },
+    { 'id': 0, 'name': 'Domingo', 'name_caps' : 'DOMINGO' },
 ];
 
 const months = [
-    { 'id': 1, 'name': 'Janeiro' },
-    { 'id': 2, 'name': 'Fevereiro' },
-    { 'id': 3, 'name': 'Março' },
-    { 'id': 4, 'name': 'Abril' },
-    { 'id': 5, 'name': 'Maio' },
-    { 'id': 6, 'name': 'Junho' },
-    { 'id': 7, 'name': 'Julho' },
-    { 'id': 8, 'name': 'Agosto' },
-    { 'id': 9, 'name': 'Setembro' },
-    { 'id': 10, 'name': 'Outubro' },
-    { 'id': 11, 'name': 'Novembro' },
-    { 'id': 12, 'name': 'Dezembro' },
+    { 'id': 1, 'name': 'Janeiro', 'name_small' : 'JAN' },
+    { 'id': 2, 'name': 'Fevereiro', 'name_small' : 'FEV' },
+    { 'id': 3, 'name': 'Março', 'name_small' : 'MAR' },
+    { 'id': 4, 'name': 'Abril', 'name_small' : 'ABR' },
+    { 'id': 5, 'name': 'Maio', 'name_small' : 'MAI' },
+    { 'id': 6, 'name': 'Junho', 'name_small' : 'JUN' },
+    { 'id': 7, 'name': 'Julho', 'name_small' : 'JUL' },
+    { 'id': 8, 'name': 'Agosto', 'name_small' : 'AGO' },
+    { 'id': 9, 'name': 'Setembro', 'name_small' : 'SET' },
+    { 'id': 10, 'name': 'Outubro', 'name_small' : 'OUT' },
+    { 'id': 11, 'name': 'Novembro', 'name_small' : 'NOV' },
+    { 'id': 12, 'name': 'Dezembro', 'name_small' : 'DEZ' },
 ];
 
 
-function formata_hora(data){
+function formata_hora_extenso(data){
     dataHora = data.split(' ');
     //var data =  window.sessionStorage.getItem('data_referencia');
     var hashdata = dataHora[1].split(":");
@@ -40,7 +40,7 @@ function formata_hora(data){
 
 
 
-function formata_dia(data){
+function formata_dia_extenso(data){
     //var data =  window.sessionStorage.getItem('data_referencia');
     dataHora = data.split(' ');
     var hashdata = dataHora[0].split("-");
@@ -74,6 +74,59 @@ function formata_dia(data){
     }
     if(strDia == "")
     strDia = D+" de "+mes;
+
+    return strDia;
+
+}
+
+
+function formata_hora_resumido(data){
+    dataHora = data.split(' ');
+    //var data =  window.sessionStorage.getItem('data_referencia');
+    var hashdata = dataHora[1].split(":");
+    H = hashdata[0];
+    m = hashdata[1];
+
+    strHora = H +':'+ m;
+    return strHora;
+}
+
+
+
+function formata_dia_resumido(data){
+    //var data =  window.sessionStorage.getItem('data_referencia');
+    dataHora = data.split(' ');
+    var hashdata = dataHora[0].split("-");
+    ano = hashdata[0];
+    mes = hashdata[1];
+    D = hashdata[2];
+    hoje = new Date();
+    S = new Date(ano,mes -1,D).getUTCDay();
+    date_ref = new  Date(ano,mes -1,D);
+    diff = new Date(date_ref - hoje);
+    diff_days = diff/1000/60/60/24;
+
+    diaName = dias.find(x => x.id === S).name_caps;
+    mes = months.find(x => x.id == mes).name_small;
+
+    strDia = "";
+    if(Math.floor(diff_days) < 7){
+        if(Math.floor(diff_days) < 2){
+        if(D == hoje.getDate()){
+            strDia = 'HOJE';
+        }
+        if(parseInt(D) == parseInt(hoje.getDate()) + 1){
+            strDia = 'AMANHÃ';
+        }
+        }else{
+        strDia = diaName;
+        }
+    }
+    if(parseInt(D) < 10){
+    D = '0'+ parseInt(D);
+    }
+    if(strDia == "")
+    strDia = D+"/"+mes;
 
     return strDia;
 
