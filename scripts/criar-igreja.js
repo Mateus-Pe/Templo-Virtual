961770a891cece74cb3cfaed1b2f8ac42d90ce5f
Mateus <mateuclash@gmail.com>
2024-09-19 17:31:49 -0300

    fazendo uma limpa no js, html e css da tela criar igreja

--- scripts/criar-igreja.js
+++ scripts/criar-igreja.js
@@ -1,71 +1,44 @@
 var paroquia_id = '';
 var myJson; 
-
 var myJsonPesq; 
-
 var igrejaId = null;
 
 
 $(document).ready(function() {
-
   paroquia_id = window.sessionStorage.getItem('paroquia_id');  
-
   getCidades();
   igrejaId = window.sessionStorage.getItem('igreja_id');
   $('#cep_instituicao').mask('00000000');
-  
-  if(igrejaId != null && igrejaId != ''){
-    carregarIgreja(igrejaId);
-  }
-  
-
 });
 
 
-
 $('#btn_salvar').click(function(e){
+  nomeAbreviado = igreja_desc_resumida($('#nome_instituicao').val());
 
-  
-  nomeAbreviado = igreja_desc_resumida_new($('#nome_instituicao').val());
- 
-    
-    if($('#nome_instituicao').val() == ''){
-      $('.cadastro__alert').text('Para prosseguir coloque uma instituição');
+  if($('#nome_instituicao').val() == ''){
+    $('.cadastro__alert').text('Para prosseguir coloque uma instituição');
+  }
+  else if($('#cep_instituicao').val() == ''){
+      $('.cadastro__alert').text('Para prosseguir coloque o CEP');
     }
-    else if($('#cep_instituicao').val() == ''){
-        $('.cadastro__alert').text('Para prosseguir coloque o CEP');
-      }
-    else if($('#cidade_instituicao').val() == ''){
-      $('.cadastro__alert').text('Para prosseguir coloque a cidade');
-    }
-    else if($('#bairro_instituicao').val() == ''){
-      $('.cadastro__alert').text('Para prosseguir coloque o bairro');
-    }
-    else if($('#logradouro_instituicao').val() == ''){
-        $('.cadastro__alert').text('Para prosseguir coloque um logradouro');
-    }
-    //else if($('#numero_instituicao').val() == ''){
-      //  $('.cadastro__alert').text('Para prosseguir coloque o número');
-    //}
-      
-    else{
-      if(igrejaId != null && igrejaId != ''){
-        atualizar();
-      }
-      else{
-        //salvar();
-        apiGeoLocation();
-      }
-      
-    // window.location = 'lista-igreja.html';
-    }     
-}); 
+  else if($('#cidade_instituicao').val() == ''){
+    $('.cadastro__alert').text('Para prosseguir coloque a cidade');
+  }
+  else if($('#bairro_instituicao').val() == ''){
+    $('.cadastro__alert').text('Para prosseguir coloque o bairro');
+  }
+  else if($('#logradouro_instituicao').val() == ''){
+      $('.cadastro__alert').text('Para prosseguir coloque um logradouro');
+  }
+  else{
+    apiGeoLocation();
+  }
+});
 
 
 $("#voltar").click(function(e){
   window.location = 'lista-igreja.html';
 });
-
 
 
 $('.tab_input').keyup(function (e) {
@@ -77,46 +50,16 @@ $('.tab_input').keyup(function (e) {
 });
 
 
-
-/*$('.adicionar_input').focus(function(e) {
-  desabilita_campos($(this).parent());
-});*/
-
-
-/*$('.adicionar_input').blur(function(e) {
-  habilita_campos();
-});*/
-
-
-
-function desabilita_campos(elemento){
-  $('.div_form').hide();
-  elemento.show();
-  
-
-}
-
 function habilita_campos(){
   $('.div_form').show();
- 
 }
 
 
 $('#cidade_instituicao').focus(function(e) {
-  
   $('.div_form').hide();
-  $('#div_cidade_instituicao').show();  
+  $('#div_cidade_instituicao').show();
   $('#divLista').show();
-  $('#div_checkbox').hide();
- 
-
-  
 });
-
-
-
-
-
 
 
 function getCidades(){
@@ -142,162 +85,107 @@ function getCidades(){
         configurarEventos();
 
       }
-
   });
-
 }
+
 
 function montaCidades(data){
 
   $('.cadastro .container .lista').html('');
-
-
-
   var rows = JSON.parse(data.length);
-
-
-
   html = "";
 
   for (var i = 0; i < rows; i++) {
 
     html +=      '<div class="divPesq" data-id="'+data[i].id+'" data-name="'+data[i].nickname+'" style="max-height:40px;margin-top: 10px; padding-bottom: 10px; border-bottom: 1px solid darkred;">';
 
-         
+    html +=         '<div style="margin-top: 10px;">' +
 
-    
+                      '<span style="font-size:1.4rem;">'+ data[i].nickname +' - '+data[i].uf+ '</span>'+
 
-                   
-
-                    
-
-    html +=       '<div style="margin-top: 10px;">' +
-
-                     '<span style="font-size:1.4rem;">'+ data[i].nickname +' - '+data[i].uf+ '</span>'+
-
-                  '</div>' +
-
-                                      
-
-                    
+                    '</div>' +
 
                  '</div>';
 
-
-
-    //$('.cadastro .container .lista').append(html);
     $('.lista').show();
-
-
   }
   $('.cadastro .container .lista').html(html);
 };
 
 
-
-
 $('#cidade_instituicao').keyup(function (e) {
 
   if($('#cidade_instituicao').val().length >= 2){
-
     myJson = myJsonPesq.filter(function(a, b) {
-
         return a['name'].toLowerCase().indexOf($('#cidade_instituicao').val().toLowerCase()) >= 0;
-
     });
-
-    
-      $('html,body').scrollTop(0);
-
+    $('html,body').scrollTop(0);
   }else{
     myJson = myJsonPesq;
   }
-
-
-
-  //$('#divLista').hide();
-
   montaCidades(myJson);// this.value);
-
   configurarEventos();
 });
 
 
 $('#cep_instituicao').keyup(function (e) {
   if (e.which == 13){
-
   ajaxCep();
-
-  }  
+  }
 });
 
 $('#cep_instituicao').blur(function (e) {
-
   ajaxCep();
-
 });
+
 
 function ajaxCep(){
   //obj = mock_cep($('#cep_instituicao').val());
   if ($('#cep_instituicao').val().length == 8){
 
   
-  url = "http://viacep.com.br/ws/"+$('#cep_instituicao').val()+"/json/";
-   
-  $.ajax({
-    method: "GET",
-    url: url
+    url = "http://viacep.com.br/ws/"+$('#cep_instituicao').val()+"/json/";
     
-  })
-  .done(function (ret) {
-      
-    if(ret != null){
+    $.ajax({
+      method: "GET",
+      url: url
+    })
+    .done(function (ret) {
+      if(ret != null){
 
-      if(ret.erro){
+        if(ret.erro){
+          $('#cidade_instituicao').focus();
+        }
+        else{
+          monta_endereco_cep(ret);
+        }
+      }else{
         $('#cidade_instituicao').focus();
       }
-      else{
-        monta_endereco_cep(ret);
-      }
-      
+    });
+  }
+}
 
-    }else{
-      $('#cidade_instituicao').focus();
-      
-    }    
-  });
-}
-}
 
 function monta_endereco_cep(obj){
-
   cidade = obj.localidade +" - "+obj.uf;
-
   objCidade = myJson.filter(function(a, b) {
-
     return a['name'].toLowerCase().indexOf(obj.localidade.toLowerCase()) >= 0;
-
   });
   if(objCidade.length > 0){
     $('#cidade_id_instituicao').val(objCidade[0].id);
-    //$('#cidade_instituicao').val(cidade) .prop('disabled', true);
-    //$('#bairro_instituicao').val(obj.bairro) .prop('disabled', true);
-    //$('#logradouro_instituicao').val(obj.logradouro) .prop('disabled', true);
     $('.div_form').show();
-
     $('#numero_instituicao').focus();
   }
-    
   else{
     $('#cep_instituicao').val("");
     $('#cep_instituicao').focus("");
     //alert("Esta Localidade ainda não existe no Servitus");
   }
-    
-   
-
 }
-function mock_cep(cep){
+
+
+/*function mock_cep(cep){
   var enderecos = [{
     "cep": "18076010",
     "cidade": "Sorocaba - SP",
@@ -329,10 +217,10 @@ function mock_cep(cep){
     return objReturn;
 
 
-}
+}*/
 
 function salvar(objGeo){
-  descricaoResumida = igreja_desc_resumida_new($('#nome_instituicao').val());
+  descricaoResumida = igreja_desc_resumida($('#nome_instituicao').val());
 
   $.ajax({
     method: "POST",
@@ -361,144 +249,31 @@ function salvar(objGeo){
         window.sessionStorage.setItem('igreja_id', obj.igreja_id);
         window.location = "configurar-perfil-igreja.html";
       }
-      
     });
-    console.log(descricaoResumida);
 }
 
 function configurarEventos(){
-
-
   $( ".divPesq" ).on( "click", function() {
     $('#divLista').hide();
     $('#cidade_instituicao').val($(this).data('name'));
     $('#cidade_id_instituicao').val($(this).data('id'));
     habilita_campos()
     $('#bairro_instituicao').focus();
-    $('#div_checkbox').show();
-
   });
 }
 
-function carregarIgreja(){
-  $.ajax({
-    method: "POST",
-    url: "https://pedeoferta.com.br/templo/index.php/welcome/get_igreja_by_id",
-    data: {
-      igreja_id : igrejaId
-     
-    }
-  })
-    .done(function (ret) {
-      var obj = jQuery.parseJSON(ret);
-      
-      if(obj.status == '1'){
-        $('#nome_instituicao').val(obj.igreja.igreja_nome);
-        $('#logradouro_instituicao').val(obj.igreja.igreja_endereco_logradouro).prop('disabled', true);
-        $('#numero_instituicao').val(obj.igreja.igreja_endereco_numero).prop('disabled', true);
-        $('#bairro_instituicao').val(obj.igreja.igreja_endereco_bairro).prop('disabled', true);
-        $('#cidade_instituicao').val(obj.igreja.igreja_endereco_cidade).prop('disabled', true);
-        $('#cep_instituicao').val(obj.igreja.igreja_endereco_cep).prop('disabled', true);
 
-      
-      }
-       
-    });
-}
-
-
-function igreja_desc_resumida_old(nome) {
-  var palavrasARemover = [
-    "santuário", "santuario","paróquia",  "paroquia", "catedral", "comunidade",
-    "basílica", "basilica","capéla","capela", "igreja", "templo",
-    "oratório","oratorio",  "mosteiro", "matriz"
-  ];
-
-  var preposicoes = ["de", "da", "das", "do", "dos"];
-//NOSSA SENHORA DO PERPETUO SOCORRO
-  var partesNome = nome.split(" ");
-
-  console.log(partesNome);
-  var palavrasParaMaior = partesNome.filter(function(palavra) {
-    return palavra.toLowerCase() !== "nossa" && palavra.toLowerCase() !== "senhora";
-  });
-
-  palavrasParaMaior = palavrasParaMaior.filter(function(palavra) {
-    return palavra.toLowerCase() !== "são";
-  });
-
-  console.log('<br>');
-  console.log(palavrasParaMaior);
-
-  if (palavrasParaMaior.length > 0) {
-    palavrasParaMaior.pop();
-  }
-
-  console.log('<br>');
-  console.log(palavrasParaMaior);
-
-  var palavrasNaoConsideradas = palavrasARemover.concat(preposicoes);
-
-  console.log('<br>');
-  console.log(palavrasNaoConsideradas);
-
-  var maiorPalavra = "";
-  for (var i = 0; i < palavrasParaMaior.length; i++) {
-    var palavra = palavrasParaMaior[i];
-    if (!palavrasNaoConsideradas.includes(palavra.toLowerCase()) && palavra.length > maiorPalavra.length) {
-      maiorPalavra = palavra;
-    }
-  }
-
-  var nomeAbreviado = "";
-
-  for (var i = 0; i < partesNome.length; i++) {
-    var palavra = partesNome[i];
-
-    if (!palavrasARemover.includes(palavra.toLowerCase()) && !preposicoes.includes(palavra.toLowerCase())) {
-
-      if (i < partesNome.length - 1 && palavra.toLowerCase() === "nossa" && partesNome[i + 1].toLowerCase() === "senhora") {
-        nomeAbreviado += "N.S.";
-        i++; 
-      } else if (palavra.toLowerCase() === "são") {
-        nomeAbreviado += "S.";
-      } else if (i === partesNome.length - 1 || palavra === maiorPalavra) {
-        nomeAbreviado += palavra;
-      } else {
-        nomeAbreviado += palavra.charAt(0).toUpperCase() + ".";
-      }
-
-      if (i < partesNome.length - 1) {
-        nomeAbreviado += " ";
-      }
-    }
-  }
-
-  nomeAbreviado = nomeAbreviado.replace(/\.\s+/g, ".");
-
-  console.log('<br>');
-  console.log(nomeAbreviado);
-
-  return  nomeAbreviado;
-}
-
-
-function igreja_desc_resumida_new(nome) {
+function igreja_desc_resumida(nome) {
   var palavrasARemover = [
     "santuário", "santuario","paróquia",  "paroquia", "catedral", "comunidade",
     "basílica", "basilica","capéla","capela", "igreja", "templo",
     "oratório","oratorio",  "mosteiro", "matriz","de", "da", "das", "do", "dos"
   ];
 
- 
   var partesNome = nome.split(" ");
-
   var palavrasNecessarias = partesNome.filter(function(palavra) {
     return !palavrasARemover.includes(palavra.toLowerCase());
   }); 
-
-  console.log('<br>');
-  console.log(palavrasNecessarias);
 
   var nomeAbreviado = "";
   var strPalNec = palavrasNecessarias.join(" ");
@@ -520,28 +295,17 @@ function igreja_desc_resumida_new(nome) {
         }else{
           nomeAbreviado += " ";
           nomeAbreviado += palavra;
-        }  
-        
-          
-        
+        }
       }
     }
   }
-  
-
   nomeAbreviado = nomeAbreviado.replace(/\.\s+/g, ".");
-
-  console.log('<br>');
-  console.log(nomeAbreviado);
-
   return  nomeAbreviado;
 }
 
+
 function apiGeoLocation(){
   //obj = mock_cep($('#cep_instituicao').val());
-  
-    
-    
     url = "https://maps.googleapis.com/maps/api/geocode/json";
     
     $.ajax({
@@ -559,20 +323,14 @@ function apiGeoLocation(){
         
         objGeo.lat =  ret.results[0].geometry.location.lat;
         objGeo.lng =  ret.results[0].geometry.location.lng;
-        
-        
       }else{
-       
         objGeo.lat =  '';
         objGeo.lng =  '';
-        
       }   
       salvar(objGeo); 
     });
-  
-    
-   
 }
+
 
 function getAddress(){
   address = '';
@@ -582,7 +340,5 @@ function getAddress(){
   }
   address += ', '+$('#bairro_instituicao').val();
   address += ', '+$('#cidade_instituicao').val();
-
-
   return address;
 }
